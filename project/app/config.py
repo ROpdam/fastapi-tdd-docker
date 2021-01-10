546f1d67e48ca9d9d74be82263e64d7f8f2c01ce
ROpdam <robinopdam@hotmail.com>
2021-01-10 13:01:19 +0100

    Initial Commit

---
+++ project/app/config.py
@@ -0,0 +1,28 @@
+# project/app/config.py
+"""
+Notes:
+We read from the environment variables instead of from a file as a file would be 
+much too slow.
+"""
+
+import logging
+import os
+
+from pydantic import BaseSettings
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
