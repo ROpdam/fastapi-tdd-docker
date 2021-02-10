a117dac3c0ad0994be994353d893b79a13c826ff
ROpdam <robinopdam@hotmail.com>
2021-02-10 20:52:36 +0100

    Continuous Integration (2.11)

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,3 +1,20 @@
+"""
+pytest automatically discovers files that start or end with 'test'.
+Test functions must begin with 'test_' classes must begin with 'Test'.
+
+Starlette's TestClient uses Requests library to make requests against FastAPI app
+Override dependencies (?): main.app.dependency_overrides[get_settings] = get_settings_override
+
+fixtures start: https://pybit.es/pytest-fixtures.html
+fixtures: https://docs.pytest.org/en/latest/fixture.html#scope-sharing-a-fixture-instance-across-tests-in-a-class-module-or-session
+pytest, get started: https://docs.pytest.org/en/latest/getting-started.html
+
+Try using the 'given, when, then' framework for writing test: https://martinfowler.com/bliki/GivenWhenThen.html
+
+Running the test: docker-compose exec web python -m pytest
+
+yield works as finalizer / teardown of your test
+"""
 # project/tests/conftest.py
 
 
