88ad2ed097a0dcfb28644fd6f8a12ea71aa70c2c
ROpdam <robinopdam@hotmail.com>
2021-02-10 21:11:31 +0100

    Changed max length flake8 to 119

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,6 +1,5 @@
 # project/app/api/summaries.py
 
-
 from typing import List
 
 from fastapi import APIRouter, HTTPException
@@ -13,8 +12,7 @@ router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) \
-                        -> SummaryResponseSchema:
+async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
 
     response_object = {"id": summary_id, "url": payload.url}
